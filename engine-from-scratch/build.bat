c8c0acaed09f0bf1ba04e1b1da9249edfa6b6c8b
Askar <rizatdinov06@mail.ru>
2026-03-11 13:36:34 +0300

    Create render

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_internal.c
 set libs=C:\Work\cgame-engine-from-scratch\lib\SDL2main.lib C:\Work\cgame-engine-from-scratch\lib\SDL2.lib C:\Work\cgame-engine-from-scratch\lib\freetype.lib
 
 CL /Zi /I C:\Work\cgame-engine-from-scratch\include %files% /link %libs% /OUT:mygame.exe
