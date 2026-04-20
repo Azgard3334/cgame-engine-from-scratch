559fbe4981680075d5dfe94d70c7fecd71f81837
Askar <rizatdinov06@mail.ru>
2026-04-20 10:13:33 +0300

    Animated sprites

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -6,7 +6,8 @@ set time=src\engine\time\time.c
 set physics=src\engine\physics\physics.c
 set array_list=src\engine\array_list\array_list.c
 set entity=src\engine\entity\entity.c
-set files=src\glad.c src\main.c src\engine\global\global.c %render% %io% %config% %input% %time% %physics% %array_list% %entity%
+set animation=src\engine\animation\animation.c
+set files=src\glad.c src\main.c src\engine\global\global.c %render% %io% %config% %input% %time% %physics% %array_list% %entity% %animation%
 set libs=C:\Work\cgame-engine-from-scratch\lib\SDL2main.lib C:\Work\cgame-engine-from-scratch\lib\SDL2.lib C:\Work\cgame-engine-from-scratch\lib\freetype.lib
 
 CL /Zi /I C:\Work\cgame-engine-from-scratch\include %files% /link %libs% /OUT:mygame.exe
